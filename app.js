8bfa418d2abfdc40e1a7ad5981e06d53bc05eaa3
RJ <fredrickalexander1727@gmail.com>
2025-02-22 01:16:26 -0800

    commits

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 import express from "express";
 const app = express();
 app.use(express.static('public'));
+app.use(express.static('views'));
 app.use(express.urlencoded({extended: true}));
+app.set('view engine','ejs');
 
-const info = [];
-const PORT = 3002;
+const contacts = [];
+const PORT = 2000;
 
 app.listen(PORT, (req,res) => {
     console.log(`Server is running at http://localhost:${PORT}`);
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/index.html`);
+    res.render(`index`);
 })
 
 app.post('/thankyou', (req,res) => {
-    info.push(req.body);
-    res.sendFile(`${import.meta.dirname}/views/thankyou.html`);
-    console.log(req.body);
+    if (req.body.fname === "" || req.body.lname === "" || req.body.email === "") {
+        res.send("Invalid Input!");
+    }
+    else {
+        const contact = {
+            firstName: req.body.fname,
+            lastName: req.body.lname,
+            jobTitle: req.body.title,
+            company: req.body.company,
+            linkedin: req.body.linkedin,
+            email: req.body.email,
+            howMeet: req.body.meet,
+            otherMeet: req.body.othermeet,
+            message: req.body.message,
+            mailing: req.body.mailing,
+            format: req.body.format,
+            dateTime: new Date()
+        }
+        contacts.push(contact);
+        console.log(contact);
+        res.render(`thankyou`,{contact});
+    }
 });
 
 app.get(`/admin`, (req,res) => {
-    res.send(info);
+    if (contacts.length === 0) {
+        res.send("No contacts!");
+    }
+    else {
+        res.send(contacts);
+    }
 })
